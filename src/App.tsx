788eefbc1653efde7b4cc6eb6ba89cc4cf5380c7
Naoki Inomata <soramoyou04@gmail.com>
2020-07-16 20:55:22 +0900

    new site

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,26 @@
-import React, { Component, FunctionComponent } from 'react';
-import { HashRouter, Switch, Route, match } from 'react-router-dom';
+import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 
-interface DetailMatchParams {
-  id: string;
-}
-
-interface DetailProps {
-  match: match<DetailMatchParams>
-}
-
-const Detail: FunctionComponent<DetailProps> = ({ match }) => (
-  <>id: {match.params.id}</>
-);
-
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-          <div className="_RoutingArea">
-            <Switch>
-              <Route exact path="/" component={() => <>Top!</>} />
-              <Route path="/detail/:id" component={Detail} />
-            </Switch>
-          </div>
-        </div>
-      </HashRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
